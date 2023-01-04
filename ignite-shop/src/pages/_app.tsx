d12b188d55cd50981012e85f8b760aab990d6a55
kelyAna <kely.lopes@vtex.com.br>
2023-01-04 10:05:52 -0300

    feat: initial structure of the ignite-shop project

---
+++ ignite-shop/src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+
+import logoImg from '../assets/logo-ignite.svg'
+import { Container, Header } from "../styles/pages/app"
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
+}
