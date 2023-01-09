acaf3a5b1993869e21c295565d17405bca300924
kelyAna <kely.lopes@vtex.com.br>
2023-01-09 11:10:32 -0300

    feat: add checkout

---
+++ ignite-shop/src/pages/api/checkout.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { stripe } from "../../lib/stripe"
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { priceId } = req.body
+
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed." })
+  }
+
+  if (!priceId) {
+    return res.status(400).json({ error: 'Price not found.' })
+  }
+
+  const checkoutSession = await stripe.checkout.sessions.create({
+    success_url: `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+    cancel_url: `${process.env.NEXT_URL}/`,
+    mode: 'payment',
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      }
+    ]
+  })
+
+  return res.status(201).json({
+    checkoutUrl: checkoutSession.url
+  })
+}
--- ignite-shop/src/pages/index.tsx
+++ ignite-shop/src/pages/index.tsx
@@ -1,18 +1,21 @@
 import Image from "next/image"
-import { useKeenSlider } from 'keen-slider/react'
-import { HomeContainer, Product } from "../styles/pages/home"
-
-import 'keen-slider/keen-slider.min.css'
-import { stripe } from "../lib/stripe"
+import { useKeenSlider } from "keen-slider/react"
+import Link from "next/link"
 import { GetStaticProps } from "next"
 import Stripe from "stripe"
+import Head from "next/head"
+
+import { HomeContainer, Product } from "../styles/pages/home"
+import "keen-slider/keen-slider.min.css"
+import { stripe } from "../lib/stripe"
+
 
 interface HomeProps {
   products: {
-    id: string;
-    name: string;
-    imageUrl: string;
-    price: number
+    id: string
+    name: string
+    imageUrl: string
+    price: string
   }[]
 }
 
@@ -21,33 +24,38 @@ export default function Home({ products }: HomeProps) {
     slides: {
       perView: 3,
       spacing: 48,
-    }
+    },
   })
 
   return (
-    <HomeContainer ref={sliderRef} className="keen-slider">
-      {
-        products?.map((product) => {
+    <>
+      <Head>
+        <title>Home | Ignite Shop</title>
+      </Head>
+      <HomeContainer ref={sliderRef} className="keen-slider">
+        {products?.map((product) => {
           return (
-            <Product key={product.id} className="keen-slider__slide">
-              <Image src={product.imageUrl} alt="" width={520} height={480} />
-      
-              <footer>
-                <strong>{product.name}</strong>
-                <span>{product.price}</span>
-              </footer>
-          </Product>
+            <Link key={product.id} href={`/product/${product.id}`} prefetch>
+              <Product className="keen-slider__slide">
+                <Image src={product.imageUrl} alt="" width={520} height={480} />
+
+                <footer>
+                  <strong>{product.name}</strong>
+                  <span>{product.price}</span>
+                </footer>
+              </Product>
+            </Link>
           )
-        })
-      }
-    </HomeContainer>
+        })}
+      </HomeContainer>
+    </>
   )
 }
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await stripe.products.list({ 
-    expand: ['data.default_price']
-   })
+  const response = await stripe.products.list({
+    expand: ["data.default_price"],
+  })
 
   const products = response.data.map((product) => {
     const price = product.default_price as Stripe.Price
@@ -56,16 +64,16 @@ export const getStaticProps: GetStaticProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        currency: 'BRL',
+      price: new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
       }).format(price.unit_amount / 100),
     }
   })
 
   return {
     props: {
-      products
+      products,
     },
     revalidate: 60 * 60 * 2,
   }
